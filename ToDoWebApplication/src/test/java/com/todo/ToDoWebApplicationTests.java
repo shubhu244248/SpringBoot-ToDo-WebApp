22087cd9f19d591d38f9f7fbba2b0a0fb4396652
Shubham Ramesh Patil <shubhu244248@gmail.com>
2024-01-18 15:31:02 +0530

    Project Initial Commit with settings

---
+++ ToDoWebApplication/src/test/java/com/todo/ToDoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
