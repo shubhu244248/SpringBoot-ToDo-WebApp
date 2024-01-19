ad4ee44c9f0a386de5f5356aa7717a1f5473d1e1
Shubham Ramesh Patil <shubhu244248@gmail.com>
2024-01-19 22:22:00 +0530

    It's enough for today I'm tired too much

---
+++ ToDoWebApplication/src/main/java/com/todo/controller/ToDoController.java
@@ -0,0 +1,23 @@
+package com.todo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.todo.service.ToDoItemService;
+
+@Controller
+public class ToDoController {
+	
+	@Autowired
+	private ToDoItemService toDoItemService;
+	
+	@GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", toDoItemService.getAll());
+        return modelAndView;
+    }
+
+}
